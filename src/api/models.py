1e8cc8ccf1275db3502613de7e9b1493b84992ef
Pedro Morais <pmoraisinvestimentos@gmail.com>
2021-06-21 20:31:59 -0300

    first commit

---
+++ src/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Account(models.Model):
+    fullName = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    document = models.CharField(max_length=255)
+    birthDate = models.DateField()
+
+    class Meta:
+        verbose_name='Account'
+        verbose_name_plural='Accounts'
